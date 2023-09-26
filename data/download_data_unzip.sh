1f58b9027ef82b75ec522c51fc52ac0f25f09a27
David Röhrscheid <drwebservice@davidroehrscheid.com>
2023-09-26 10:37:33 +0900

    Extract original data to correct folder

--- data/download_data_unzip.sh
+++ data/download_data_unzip.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 curl "https://elaws.e-gov.go.jp/download?file_section=1&only_xml_flag=true" -A "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0" --output all_xml.zip
-unzip all_xml.zip
+unzip all_xml.zip -d all_xml
